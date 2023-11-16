85c4105f20ced4ecd318e723c1559103d54acde7
Darth Vader <danieltarancon@gmail.com>
2023-11-16 20:34:04 +0100

    16 2034

--- routes/coursesRoutes.js
+++ routes/coursesRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-// Import your Course and User models or any required dependencies here
+
+// Importing the Course and User models and any other required dependencies here
 const Course = require('../models/course');
 const User = require('../models/user');
 
-// Middleware for authentication (if needed)
 
-// GET /api/courses - Return all courses with associated users
+
+// GET /api/courses - Returning all courses with associated users
 router.get('/api/courses', async (req, res) => {
   try {
     const courses = await Course.findAll({
@@ -23,7 +24,10 @@ router.get('/api/courses', async (req, res) => {
   }
 });
 
-// GET /api/courses/:id - Return a specific course with associated user
+
+
+
+// GET /api/courses/:id - Returning a specific course with associated user
 router.get('/api/courses/:id', async (req, res) => {
   const courseId = req.params.id;
   try {
@@ -46,15 +50,17 @@ router.get('/api/courses/:id', async (req, res) => {
 
 
 
-// POST /api/courses - Create a new course with validation
+
+
+// POST /api/courses - Creating a new course with validation
 router.post('/api/courses', async (req, res) => {
-  // Get the course data from the request body
+  // Getting the course data from the request body
   const courseData = req.body;
 
-  // Create an array to store validation errors
+  // Creating an array to store validation errors
   const errors = [];
 
-  // Validate the required fields
+  // Validating the required fields
   if (!courseData.title) {
     errors.push('Please provide a value for "title"');
   }
@@ -63,22 +69,22 @@ router.post('/api/courses', async (req, res) => {
     errors.push('Please provide a value for "description"');
   }
 
-  // Check if there are any validation errors
+  // Checking if there are any validation errors
   if (errors.length > 0) {
-    // Return a 400 Bad Request status code with the validation errors
+    // Returning a 400 Bad Request status code with the validation errors
     res.status(400).json({ errors });
   } else {
-    // Implement course creation logic here if validation passes
+    // Implementing course creation logic here if validation passes
     try {
       const newCourse = await Course.create({
         title: courseData.title,
         description: courseData.description,
         // Other course properties...
-        userId: courseData.userId, // Assuming you have a userId associated with the course
+        userId: courseData.userId,
       });
-      // Set the Location header to the URI for the newly created course
+      // Setting the Location header to the URI for the newly created course
       res.location(`/api/courses/${newCourse.id}`);
-      // Return a 201 Created status code and no content
+      // Returning a 201 Created status code and no content
       res.status(201).end();
     } catch (error) {
       console.error(error);
@@ -89,16 +95,18 @@ router.post('/api/courses', async (req, res) => {
 
 
 
-// PUT /api/courses/:id - Update a specific course with validation
+
+
+// PUT /api/courses/:id - Updating a specific course with validation
 router.put('/api/courses/:id', async (req, res) => {
   const courseId = req.params.id;
-  // Get the course data from the request body
+  // Getting the course data from the request body
   const courseData = req.body;
 
-  // Create an array to store validation errors
+  // Creating an array to store validation errors
   const errors = [];
 
-  // Validate the required fields
+  // Validating the required fields
   if (!courseData.title) {
     errors.push('Please provide a value for "title"');
   }
@@ -107,24 +115,23 @@ router.put('/api/courses/:id', async (req, res) => {
     errors.push('Please provide a value for "description"');
   }
 
-  // Check if there are any validation errors
+  // Checking if there are any validation errors
   if (errors.length > 0) {
-    // Return a 400 Bad Request status code with the validation errors
+    // Returning a 400 Bad Request status code with the validation errors
     res.status(400).json({ errors });
   } else {
-    // Implement course update logic here if validation passes
+    // Implementing course update logic here if validation passes
     try {
       const course = await Course.findByPk(courseId);
       if (!course) {
         res.status(404).json({ message: 'Course not found' });
       } else {
         await course.update({
-          // Update course properties using req.body
+          // Updating course properties using req.body
           title: courseData.title,
           description: courseData.description,
-          // Other course properties...
         });
-        // Return a 204 HTTP status code and no content
+        // Returning a 204 HTTP status code and no content
         res.status(204).end();
       }
     } catch (error) {
@@ -137,17 +144,17 @@ router.put('/api/courses/:id', async (req, res) => {
 
 
 
-// DELETE /api/courses/:id - Delete a specific course
+// DELETE /api/courses/:id - Deleting a specific course
 router.delete('/api/courses/:id', async (req, res) => {
   const courseId = req.params.id;
-  // Implement course deletion logic here
+  // Implementing course deletion logic here
   try {
     const course = await Course.findByPk(courseId);
     if (!course) {
       res.status(404).json({ message: 'Course not found' });
     } else {
       await course.destroy();
-      // Return a 204 HTTP status code and no content
+      // Returning a 204 HTTP status code and no content
       res.status(204).end();
     }
   } catch (error) {
@@ -155,5 +162,9 @@ router.delete('/api/courses/:id', async (req, res) => {
     res.status(500).json({ message: 'An error occurred during course deletion' });
   }
 });
+
+
+
+
 
 module.exports = router;
