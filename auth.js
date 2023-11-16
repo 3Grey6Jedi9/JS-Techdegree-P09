b081c682a4976ce8cc071cd263bc82b835613de4
Darth Vader <danieltarancon@gmail.com>
2023-11-16 20:16:06 +0100

    16 2016

--- auth.js
+++ auth.js
@@ -1,30 +1,37 @@
 const auth = require('basic-auth');
 const bcrypt = require('bcryptjs');
-const { User } = require('./models/user'); // Import your User model here
+const { User } = require('./models/user'); // Importing the User model here
+
+
+
 
 const authenticateUser = async (req, res, next) => {
-  // Parse the user's credentials from the Authorization header
+  // Parsing the user's credentials from the Authorization header
   const credentials = auth(req);
 
   if (credentials) {
     const { name, pass } = credentials;
 
-    // Find the user by their email address
+    // Finding the user by their email address
     const user = await User.findOne({ where: { emailAddress: name } });
 
-    // Check if the user exists and if the password matches
+    // Checking if the user exists and if the password matches
     if (user && bcrypt.compareSync(pass, user.password)) {
-      // If authentication is successful, add the user to the request object
+      // If authentication is successful, we add the user to the request object
       req.currentUser = user;
-      next(); // Continue processing the request
+      next(); // Continuing processing the request
     } else {
-      // If authentication fails, return a 401 Unauthorized status
+      // If authentication fails, we will return a 401 Unauthorized status
       res.status(401).json({ message: 'Access Denied' });
     }
   } else {
-    // If no credentials are provided, return a 401 Unauthorized status
+    // If no credentials are provided, we will return a 401 Unauthorized status
     res.status(401).json({ message: 'Access Denied' });
   }
 };
+
+
+
+
 
 module.exports = authenticateUser;
